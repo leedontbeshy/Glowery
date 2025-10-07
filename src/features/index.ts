c8885f9eb7425ea816499f7edc468815ced545bd
leedontbeshy <duyphucle.0510@gmail.com>
2025-10-07 21:18:04 +0700

    feat(auth): implement authentication with JWT

--- src/features/index.ts
+++ src/features/index.ts
@@ -1,1 +1,12 @@
 //Gộp các route từ tất cả feature
+import { Router } from "express";
+import authRoutes from "./auth/auth.route";
+//import userRoutes from "./auth/user.route";
+
+const router = Router();
+
+router.use("/auth", authRoutes);
+//router.use("/users", userRoutes);
+
+
+export default router;
