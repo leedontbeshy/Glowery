bdcb49a5e7826a1e2d726b4cca3c637dd6f1d9e4
leedontbeshy <duyphucle.0510@gmail.com>
2025-10-07 12:16:38 +0700

    feat: setup TypeScript, DB schema, and user model with enums

---
+++ config/database.ts
@@ -0,0 +1,1 @@
+import PG from "pg";
---
+++ src/models/enums.ts
@@ -0,0 +1,15 @@
+export type UserRole = 'user' | 'seller' | 'admin';
+export type UserStatus = 'active' | 'inactive' | 'suspended';
+
+export type ProductStatus = 'active' | 'inactive' | 'out_of_stock';
+
+export type OrderStatus = 'pending' | 'confirmed' | 'processing' | 'shipped' | 'delivered' | 'cancelled' | 'refunded';
+
+export type PaymentMethod = 'cod' | 'bank_transfer' | 'vnpay' | 'momo' | 'zalopay' | 'credit_card';
+export type PaymentStatus = 'pending' | 'processing' | 'completed' | 'failed' | 'refunded' | 'cancelled';
+
+export type DiscountType = 'percent' | 'fixed';
+
+export type NoficationType = 'order'| 'promotion'| 'review'| 'system'| 'payment';
+
+
