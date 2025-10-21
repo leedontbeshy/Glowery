931c3c8c5bb90cdf756e1e82fac7bb7ea6fdfea7
leedontbeshy <duyphucle.0510@gmail.com>
2025-10-21 08:34:25 +0700

    refactor: simplify AdminController and AdminRepository by removing redundant error handling

--- src/features/admin/admin.controller.ts
+++ src/features/admin/admin.controller.ts
@@ -1,20 +1,21 @@
 import { Request, Response, NextFunction } from "express";
 
+import { handleControllerError } from "@/common/utils/controllerHelper";
+
 import { AdminService } from "./admin.service";
 
+
 export const AdminController = {
-    async getAllUser(req: Request, res: Response){
+    async getAllUser(req: Request, res: Response) {
         try {
             const result = await AdminService.getAllUser(req.query);
             res.status(200).json({
-                message:"Fetched users successfully",
+                message: "Fetched users successfully",
                 result
             })
         } catch (error: any) {
-                return res.status(400).json({
-                error: error.message,
-            });
+            return handleControllerError(error, res, 'getAllUser');
         }
+
     }
-    
 }
